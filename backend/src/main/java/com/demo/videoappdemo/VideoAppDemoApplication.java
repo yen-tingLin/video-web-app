47844ca74e068da12d6f7894e36a53a4edd4b545
yen-tingLin <aillvone@gmail.com>
2021-10-03 22:10:08 +0800

    generate backend and frontend modules

---
+++ backend/src/main/java/com/demo/videoappdemo/VideoAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.videoappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoAppDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/demo/videoappdemo/VideoAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.videoappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
